bb1b53cdd473edddf319ed98e5f7887d4672e0e7
Erick Shaffer <erick.shaffer321@gmail.com>
2025-01-03 15:02:16 -0700

    inital commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+import { Avatar } from '@/components/avatar'
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from '@/components/dropdown'
+import { Navbar, NavbarDivider, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer } from '@/components/navbar'
+import {
+  ArrowRightStartOnRectangleIcon,
+  ChevronDownIcon,
+  Cog8ToothIcon,
+  LightBulbIcon,
+  PlusIcon,
+  ShieldCheckIcon,
+  UserIcon,
+} from '@heroicons/react/16/solid'
+import { InboxIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
-export default function Home() {
+function Example() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <Navbar>
+      <Dropdown>
+        <DropdownButton as={NavbarItem}>
+          <Avatar src="/tailwind-logo.svg" />
+          <NavbarLabel>Tailwind Labs</NavbarLabel>
+          <ChevronDownIcon />
+        </DropdownButton>
+        <DropdownMenu className="min-w-64" anchor="bottom start">
+          <DropdownItem href="/teams/1/settings">
+            <Cog8ToothIcon />
+            <DropdownLabel>Settings</DropdownLabel>
+          </DropdownItem>
+          <DropdownDivider />
+          <DropdownItem href="/teams/1">
+            <Avatar slot="icon" src="/tailwind-logo.svg" />
+            <DropdownLabel>Tailwind Labs</DropdownLabel>
+          </DropdownItem>
+          <DropdownItem href="/teams/2">
+            <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
+            <DropdownLabel>Workcation</DropdownLabel>
+          </DropdownItem>
+          <DropdownDivider />
+          <DropdownItem href="/teams/create">
+            <PlusIcon />
+            <DropdownLabel>New team&hellip;</DropdownLabel>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavbarDivider className="max-lg:hidden" />
+      <NavbarSection className="max-lg:hidden">
+        <NavbarItem href="/" current>
+          Home
+        </NavbarItem>
+        <NavbarItem href="/events">Events</NavbarItem>
+        <NavbarItem href="/orders">Orders</NavbarItem>
+      </NavbarSection>
+      <NavbarSpacer />
+      <NavbarSection>
+        <NavbarItem href="/search" aria-label="Search">
+          <MagnifyingGlassIcon />
+        </NavbarItem>
+        <NavbarItem href="/inbox" aria-label="Inbox">
+          <InboxIcon />
+        </NavbarItem>
+        <Dropdown>
+          <DropdownButton as={NavbarItem}>
+            <Avatar src="/profile-photo.jpg" square />
+          </DropdownButton>
+          <DropdownMenu className="min-w-64" anchor="bottom end">
+            <DropdownItem href="/my-profile">
+              <UserIcon />
+              <DropdownLabel>My profile</DropdownLabel>
+            </DropdownItem>
+            <DropdownItem href="/settings">
+              <Cog8ToothIcon />
+              <DropdownLabel>Settings</DropdownLabel>
+            </DropdownItem>
+            <DropdownDivider />
+            <DropdownItem href="/privacy-policy">
+              <ShieldCheckIcon />
+              <DropdownLabel>Privacy policy</DropdownLabel>
+            </DropdownItem>
+            <DropdownItem href="/share-feedback">
+              <LightBulbIcon />
+              <DropdownLabel>Share feedback</DropdownLabel>
+            </DropdownItem>
+            <DropdownDivider />
+            <DropdownItem href="/logout">
+              <ArrowRightStartOnRectangleIcon />
+              <DropdownLabel>Sign out</DropdownLabel>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarSection>
+    </Navbar>
+  )
 }
